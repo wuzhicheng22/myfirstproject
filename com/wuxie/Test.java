188bb27e5b823f04008e5c708208422bfa49320a
wuzhicheng22 <2021115923@qq.com>
2025-03-03 00:32:58 +0800

    feat: hello world111

--- com/wuxie/Test.java
+++ com/wuxie/Test.java
@@ -4,5 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("aaaaaa");
+        System.out.println("Hello world");
+        System.out.println("aaaaaa");
     }
 }
